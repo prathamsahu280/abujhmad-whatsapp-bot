735dd01341d1cc7ee573563efc91446e91132461
prathamsahu280 <prathamsahu2809@gmail.com>
2025-01-24 19:20:01 +0700

    changes

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ app.post('/send-marathon-message', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
